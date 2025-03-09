7400ce054f76c95e985251fae84d671171e2de75
Mor Talla DIA <mortalladia292@gmail.com>
2025-03-09 18:04:35 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,22 +27,7 @@ resource "aws_s3_bucket_public_access_block" "portfolio" {
   restrict_public_buckets = false
 }
 
-# Politique bucket séparée
-resource "aws_s3_bucket_policy" "portfolio" {
-  bucket = aws_s3_bucket.portfolio_bucket.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.portfolio_bucket.arn}/*"
-      }
-    ]
-  })
-}
 resource "aws_cloudfront_origin_access_identity" "portfolio" {
   comment = "Accès sécurisé via CloudFront"
 }
