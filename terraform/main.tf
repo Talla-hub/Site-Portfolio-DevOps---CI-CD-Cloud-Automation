0d3b2d9aa366708af108b186cb84af1dd9a35d2b
Mor Talla DIA <mortalladia292@gmail.com>
2025-03-09 10:44:07 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 # Créer un bucket S3 pour héberger le site
 resource "aws_s3_bucket" "portfolio_bucket" {
-  bucket = "mon-portfolio-devops-123"
+  bucket = "mon-portfolio-devops-1234"
   #acl    = "public-read"
   # Ajoutez une politique de bucket pour gérer les autorisations d'accès
   policy = jsonencode({
